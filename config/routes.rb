381437ec47ba9e6a504d595a9fd5c1cdb186dd53
danny <danny.code1@gmail.com>
2014-08-24 16:06:48 +0900

    add tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :tasks, only: [:index]
+  root 'tasks#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
